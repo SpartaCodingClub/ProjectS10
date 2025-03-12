cb7fb6ee25d8201b20b9b20edb755ed7c64dbf4d
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-03-13 01:33:04 +0900

    [feat] 박성준_장비 개발 중

--- Assets/Scripts/Player/P_Equipment.cs
+++ Assets/Scripts/Player/P_Equipment.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class P_Equipment : MonoBehaviour
 {
+    [SerializeField] float range;
+    [SerializeField] Equipment curEquipment;
+    [SerializeField] Vector3 boxOffset;
+    [SerializeField] LayerMask enemyLayer;
+    Vector3 boxSize = new Vector3(1, 1, 1);
+    RaycastHit[] hits;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,17 @@ public class P_Equipment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (curEquipment != null)
+        {
+            hits = Physics.BoxCastAll(transform.position + transform.forward * range / 2 + boxOffset, boxSize * range, transform.forward, transform.rotation, 0, enemyLayer);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Vector3 boxCenter = transform.position + transform.forward * (range / 2) + boxOffset;
+        Gizmos.matrix = Matrix4x4.TRS(boxCenter, transform.rotation, boxSize * range);
+        Gizmos.DrawWireCube(Vector3.zero, boxSize);
     }
 }
---
+++ Assets/TEMP/PSJ/Script/Equipment.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+    Melee,
+    Projectile
+}
+public class Equipment : MonoBehaviour
+{
+    public float atk;
+    public float dfs;
+    public float atkDelay;
+    public float range;
+
+    public void MeleeAttack()
+    {
+        
+    }
+}
