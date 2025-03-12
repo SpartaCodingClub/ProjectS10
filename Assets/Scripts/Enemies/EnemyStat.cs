6304afbae2f6148d5c9b9b4f8eadc005f6b9073e
DDanPat <hyoung6855@gmail.com>
2025-03-12 20:45:39 +0900

    [Feat] 정형권_적 AI 작업중

--- Assets/Scripts/Enemies/EnemyStat.cs
+++ Assets/Scripts/Enemies/EnemyStat.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStat : StatHandler
 {
+    private NavMeshAgent agent;
 
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+        agent.speed = Speed;
+    }
 }
