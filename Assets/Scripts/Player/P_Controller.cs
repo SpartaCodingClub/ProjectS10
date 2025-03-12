3ca4150967814764d2714ad58f6984394c7ba90a
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-03-12 21:04:10 +0900

    [fix] 박성준_플레이어 스크립트 수정
    스탯 핸들러 기반으로 움직이도록.

--- Assets/Scripts/Player/P_Controller.cs
+++ Assets/Scripts/Player/P_Controller.cs
@@ -14,7 +14,6 @@ public class PlayerController : MonoBehaviour
     Camera cam;
 
     [Header("이동")]
-    [SerializeField] float moveSpeed = 3f;
     [SerializeField] float curSpeed;
     [SerializeField] float speedChangeValue;
     [SerializeField] float MoveAngle;
@@ -80,7 +79,7 @@ public class PlayerController : MonoBehaviour
         float targetSpeed = curMovementInput.magnitude;
         curSpeed = Mathf.Lerp(curSpeed, targetSpeed, Time.deltaTime * speedChangeValue);
         Vector3 direction = Vector3.forward * curMovementInput.y + Vector3.right * curMovementInput.x;
-        direction *= moveSpeed;
+        direction *= PStat.Speed;
         direction.y = 0;
 
         float angle = Vector3.Angle(transform.forward, direction);
