bbe37e83ed282ae68fbe9671d1d3568862b52d62
Due <kcy7th@naver.com>
2025-03-13 12:52:18 +0900

    [Feat] 이 수_BuildingBase, BuildingWall

--- Assets/Scripts/Buildings/BuildingBase.cs
+++ Assets/Scripts/Buildings/BuildingBase.cs
@@ -2,17 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingBase : MonoBehaviour
+public class BuildingBase : Poolable
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("건물 기본 설정")]
+    public float health = 100f;
+    public float maxHealth = 100f;
+    protected bool isConstructed = false;
+
+    public virtual void Initialize()
     {
-        
+        health = maxHealth;
+        isConstructed = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void TakeDamage(float damage)
     {
-        
+        health -= damage;
+        if (health <= 0)
+        {
+            DestroyBuilding();
+        }
+    }
+
+    public virtual void DestroyBuilding()
+    {
+        Managers.Resource.Destroy(gameObject);
+    }
+
+    public virtual void StartConstruction(float buildTime)
+    {
+        StartCoroutine(Construct(buildTime));
+    }
+
+    private IEnumerator Construct(float buildTime)
+    {
+        float timer = 0f;
+        while (timer < buildTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        isConstructed = true;
     }
 }
