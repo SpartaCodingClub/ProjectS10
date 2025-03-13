cbe565a1510852af29fab95f02c86cdfe02d670d
tmdrb7214 <tmdrb7214@naver.com>
2025-03-13 20:56:04 +0900

    feat 사용가능한맵에 resource 생성
    사용가능한맵에 resource 생성

--- Assets/Scripts/Objects/ResourceObject.cs
+++ Assets/Scripts/Objects/ResourceObject.cs
@@ -11,63 +11,71 @@ public class ResourceObject : InteractableObject
     public int capacity; // 총 몇번때릴수있는지 
     public int resourceumber;
 
-    public float respawnTime;
     private Vector3 respawnArea;
 
-    private void Awake()
+    Map map;
+
+    private void Start()
     {
-        spawn();
+        map = FindAnyObjectByType<Map>();
+        if (map != null) Debug.Log("맵이할당되었습니다");
+        StartCoroutine(Respawn(0.1f));
     }
 
-    //public void Gather(Vector3 hitPoint , Vector3 hitNormal)
-    //{
-    //    for (int i = 0; i < quantityperhit; i++) 
-    //    {
-    //        if (capacity <= 0)
-    //        {
-    //            DestroyResource();
-    //            break;
-    //        }
 
-    //        capacity -= 1;
-    //        //Instantiate(itemToGive.dropPrefab, hitPoint + Vector3.up, Quaternion.LookRotation(hitNormal, Vector3.up));
-    //    }
-    //}
+
+    public void Gather(Vector3 hitPoint, Vector3 hitNormal)
+    {
+        for (int i = 0; i < quantityperhit; i++)
+        {
+            if (capacity <= 0)
+            {
+                DestroyResource();
+                Debug.Log("Destroy");
+                break;
+            }
+
+            capacity -= 1;
+            //Instantiate(itemToGive.dropPrefab, hitPoint + Vector3.up, Quaternion.LookRotation(hitNormal, Vector3.up));
+        }
+    }
 
     void DestroyResource()
     {
         Managers.Resource.Destroy(resource);
-        StartCoroutine(Respawn());
+        StartCoroutine(Respawn(30f));
     }
-
-    IEnumerator Respawn()
+    IEnumerator Respawn(float respwanTime)
     {
+        yield return new WaitForSeconds(respwanTime);
+        Vector3 randomPos = map.GetRandomPosition();
+        Instantiate();
+        //Vector3 randomPosition = new Vector3(randomPosition.x = Random.Range(respawnArea.x - 30, respawnArea.x + 30),
+        //randomPosition.y = respawnArea.y,
+        //randomPosition.z = Random.Range(respawnArea.z = 0, respawnArea.z + 30));
 
-        yield return new WaitForSeconds(respawnTime);
-
-        Vector3 randomPosition = new Vector3(randomPosition.x = Random.Range(respawnArea.x - 14, respawnArea.x + 14),
-        randomPosition.y = respawnArea.y,
-        randomPosition.z = Random.Range(respawnArea.z - 14, respawnArea.z + 14));
-
-
-
-        resource.transform.position = randomPosition;
-        Managers.Resource.Instantiate(resource);
+        resource.transform.position = randomPos;
+        //Managers.Resource.Instantiate(resource);
         Debug.Log(resource);
         Debug.Log(resource.transform.position);
-
     }
 
-    private void spawn()
+    private void Instantiate()
     {
-
-            Vector3 randomPosition = new Vector3(randomPosition.x = Random.Range(respawnArea.x - 14, respawnArea.x + 14),
-            randomPosition.y = respawnArea.y,
-            randomPosition.z = Random.Range(respawnArea.z - 14, respawnArea.z + 14));
-
-            resource.transform.position = randomPosition;
+        GameObject.Instantiate(resource);
+    }
+    private void Spawn()
+    {
         for (int i = 0; i < resourceumber; i++)
         {
+            Vector3 randomPos = map.GetRandomPosition();
+
+            randomPos = new Vector3( Random.Range(respawnArea.x - 30, respawnArea.x + 30),
+            randomPos.y = respawnArea.y,
+            randomPos.z = Random.Range(respawnArea.z = 0, respawnArea.z + 30));
+
+            resource.transform.position = randomPos;
+
             Managers.Resource.Instantiate(resource);
             Debug.Log(resource);
             Debug.Log(resource.transform.position);
