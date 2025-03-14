ea44633c45f73a3ec77ea27176e6ffdd6e3b65b6
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-03-14 14:22:56 +0900

    [Feat] 송석호_PlayerCondition UI 수정

--- Assets/Scripts/Player/P_Stat.cs
+++ Assets/Scripts/Player/P_Stat.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem.XR.Haptics;
 
 public class P_Stat : StatHandler
 {
@@ -11,25 +7,15 @@ public class P_Stat : StatHandler
 
     public delegate Action damageaction();
     public damageaction DamageAction;
-    private UI_HealthBar healthBar;
-    // Start is called before the first frame update
+
     void Start()
     {
-        healthBar = gameObject.FindComponent<UI_HealthBar>();
-        healthBar.UpdateUI(Health, 100);
         PlusSpeed = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void Damage(float damage)
     {
         Health -= damage;
-        healthBar.UpdateUI(Health, 100);
         DamageAction();
     }
 }
