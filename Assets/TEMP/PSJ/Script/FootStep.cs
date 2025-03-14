b4202ddf5e6745ba8e5a6aaa6c16505c080d7e78
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-03-14 16:06:27 +0900

    [feat] 박성준_발소리 스크립트

---
+++ Assets/TEMP/PSJ/Script/FootStep.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStep : MonoBehaviour
+{
+    Animator animator;
+    float _lastFootstep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!animator) animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var footstep = animator.GetFloat("FootStep");
+
+        if (_lastFootstep > 0 && footstep < 0 || _lastFootstep < 0 && footstep > 0)
+        {
+            Debug.Log("발자국!");
+        }
+
+        _lastFootstep = footstep;
+    }
+}
