ede0a3bb5d8fffb62636dc436fff540f2158966c
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-03-18 14:08:30 +0900

    [fix] 박성준_애니핸들러 작동 방식 수정

--- Assets/Scripts/Player/P_AniHandler.cs
+++ Assets/Scripts/Player/P_AniHandler.cs
@@ -70,9 +70,18 @@ public class P_AniHandler : MonoBehaviour
     IEnumerator PlayAni(string input)
     {
         animator.CrossFade(input, 0.1f);
-        yield return null;
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-        yield return new WaitForSeconds(stateInfo.length);
+
+        while (!animator.GetCurrentAnimatorStateInfo(0).IsName(input))
+        {
+            yield return null;
+        }
+
+        float length = animator.GetCurrentAnimatorStateInfo(0).length;
+        float speed = animator.GetCurrentAnimatorStateInfo(0).speed;
+        float adjustedLength = length / speed;
+
+        yield return new WaitForSeconds(adjustedLength);
+
         isAnimationing = false;
     }
 
