336eceb4598970a47f3d758bde31572267396adf
SpartaCodingClub <spartacodingclub.kr@gmail.com>
2025-03-18 17:24:20 +0900

    [Fix] 송석호_적 감지 오류 수정

--- Assets/Scripts/Buildings/TurretRotation.cs
+++ Assets/Scripts/Buildings/TurretRotation.cs
@@ -7,20 +7,18 @@ public class TurretRotation : MonoBehaviour
 
     public void EnableRotation()
     {
-        Debug.Log("Turret Rotation Enabled!");
-        InvokeRepeating("FindTarget", 0f, 0.5f); 
+        InvokeRepeating("FindTarget", 0f, 0.5f);
     }
 
     private void FindTarget()
     {
         // Enemy 태그 인식 후 회전
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        EnemyController[] gameObjects = FindObjectsOfType<EnemyController>();
+
         float closestDistance = Mathf.Infinity;
         Transform closestEnemy = null;
 
-        Debug.Log($"적 개수: {enemies.Length}");
-
-        foreach (GameObject enemy in enemies)
+        foreach (var enemy in gameObjects)
         {
             float distance = Vector3.Distance(transform.position, enemy.transform.position);
 
@@ -38,15 +36,13 @@ public class TurretRotation : MonoBehaviour
     {
         if (target == null)
         {
-            Debug.Log("타겟 없음");
             return;
         }
 
         Vector3 direction = target.position - transform.position;
 
-        if (direction == Vector3.zero) 
+        if (direction == Vector3.zero)
         {
-            Debug.LogWarning("Look rotation viewing vector is zero. 회전할 방향이 없습니다!");
             return;
         }
 
